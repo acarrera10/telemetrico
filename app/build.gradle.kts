c3aa99dfabe51a582a71eddb83f28db422edbb94
acarrera10 <alejandro@trescientosuno.com.ar>
2026-09-13 18:17:03 -0300

    Bump app to v0.4.2-test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.telemetrico.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1-test"
+        versionCode = 6
+        versionName = "0.4.2-test"
     }
 
     buildTypes {
