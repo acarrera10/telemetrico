1b1ad4c588b9b27531d0134bab98765266048f80
acarrera10 <alejandro@trescientosuno.com.ar>
2026-09-20 19:05:08 -0300

    Prepare v0.4.3.5-test perimeter frame release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.telemetrico.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.4.3.4-test"
+        versionCode = 12
+        versionName = "0.4.3.5-test"
     }
 
     signingConfigs {
